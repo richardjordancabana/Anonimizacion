b1e27fea0efc8439f1ce3ebadd5685b25e5f0b72
richard <richard@192.168.1.42>
2015-10-14 03:29:50 +0200

    prueba inicial

---
+++ src/anonimizacion/Anonimizacion.java
@@ -0,0 +1,23 @@
+
+package anonimizacion;
+
+/**
+ *
+ * @author richard
+ */
+public class Anonimizacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+   
+        MySql db = new MySql();
+        db.MySQLConnection("root", "", "");
+    
+
+        
+    }
+    
+}
---
+++ src/anonimizacion/MySql.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anonimizacion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author richard
+ */
+
+public class MySql {
+    
+    private static Connection Conexion;
+    
+    //conexión con la bbdd
+    public void MySQLConnection(String user, String pass, String db_name) throws Exception {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + db_name, user, pass);
+            JOptionPane.showMessageDialog(null, "Se ha iniciado la conexión con el servidor de forma exitosa");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MySql.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(MySql.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+  
+    //cerrar la conexión
+     public void closeConnection() {
+        try {
+            Conexion.close();
+            JOptionPane.showMessageDialog(null, "Se ha finalizado la conexión con el servidor");
+        } catch (SQLException ex) {
+            Logger.getLogger(MySql.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+   
+     //crea una bbdd
+     public void createDB(String name) throws Exception {
+        try {
+            String Query = "CREATE DATABASE " + name;
+            Statement st = Conexion.createStatement();
+            st.executeUpdate(Query);
+            closeConnection();
+            MySQLConnection("root", "", name);
+            JOptionPane.showMessageDialog(null, "Se ha creado la base de datos " + name + " de forma exitosa");
+        } catch (SQLException ex) {
+            Logger.getLogger(MySql.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+     
+     //crea una tabla
+      public void createTable(String name) {
+        try {
+            String Query = "CREATE TABLE " + name + ""
+                    + "(ID VARCHAR(25),Nombre VARCHAR(50), Apellido VARCHAR(50),"
+                    + " Edad VARCHAR(3), Sexo VARCHAR(1))";
+
+            Statement st = Conexion.createStatement();
+            st.executeUpdate(Query);
+            JOptionPane.showMessageDialog(null, "Se ha creado la tabla " + name + " de forma exitosa");
+        } catch (SQLException ex) {
+            Logger.getLogger(MySql.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
